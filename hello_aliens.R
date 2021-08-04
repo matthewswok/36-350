ca8cc693df6f078997ff3a43f513b63d49618ba2
Matthew Ok <matthew.sw.ok@gmail.com>
2021-08-04 13:07:23 -0400

    Project Q17

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+cat("We send greetings from planet Earth, 
+    which orbits the Star we call the Sun, 
+    located on the Orion Arm of the Milky Way.
+    We a complex species capable of both peace and conflict. 
+    We seek new technologies and also wish to share our
+    scientific knowledge of the universe to others who
+    know less than we do. 
+    We are also a curious species. We have been exploring
+    the cosmos and believe we are not alone in the universe.")
